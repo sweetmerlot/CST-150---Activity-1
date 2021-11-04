4c09d097c81fe37c10246245a72de582657d2b9b
Jennifer <jnarvaez07@gmail.com>
2021-11-04 18:09:04 -0500

    first commit

---
+++ MyFirstProject/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace MyFirstProject
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.MessageButton = new System.Windows.Forms.Button();
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.answerLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MessageButton
+            // 
+            this.MessageButton.Location = new System.Drawing.Point(136, 14);
+            this.MessageButton.Name = "MessageButton";
+            this.MessageButton.Size = new System.Drawing.Size(92, 35);
+            this.MessageButton.TabIndex = 0;
+            this.MessageButton.Text = "Display Message";
+            this.MessageButton.UseVisualStyleBackColor = true;
+            this.MessageButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.Location = new System.Drawing.Point(142, 66);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(95, 13);
+            this.questionLabel.TabIndex = 1;
+            this.questionLabel.Text = "This is a question?";
+            // 
+            // answerLabel
+            // 
+            this.answerLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.answerLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.answerLabel.Location = new System.Drawing.Point(142, 90);
+            this.answerLabel.Name = "answerLabel";
+            this.answerLabel.Size = new System.Drawing.Size(95, 13);
+            this.answerLabel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Show Answer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 220);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.answerLabel);
+            this.Controls.Add(this.questionLabel);
+            this.Controls.Add(this.MessageButton);
+            this.Name = "Form1";
+            this.Text = "My First Program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+        #endregion
+
+        private System.Windows.Forms.Button MessageButton;
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.Label answerLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MyFirstProject/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstProject
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			MessageBox.Show("Here is a Message!");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            answerLabel.Text = "This is an Answer";
+        }
+    }
+}
